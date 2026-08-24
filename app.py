0bbd3d5c2ec46250f6c9b95f55024833f7faf1cb
Rafael Flores <rafael845jr@gmail.com>
2026-08-23 21:21:45 -0500

    Signed-off-by: Rafael Flores <rafael845jr@gmail.com>

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ ventana.geometry("420x320")
 ventana.configure(bg="#1e3a5f")
  
 ventana.mainloop()
+
+from tkinter import messagebox
+ 
+def mostrar_mensaje():
+    messagebox.showinfo(
+        "Mensaje",
+        "Hola, este es un proyecto de ejemplo para Git GUI."
+    )
+ 
+boton_mensaje = tk.Button(
+    ventana,
+    text="Mostrar mensaje",
+    command=mostrar_mensaje,
+)
+boton_mensaje.pack(pady=10)
